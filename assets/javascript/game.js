a982bb841e62151364cbc05b87892194571e04a5
Jeremy Pena <jpena.boots@gmail.com>
2020-04-25 09:26:46 -0700

    assets folder update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -201,32 +201,6 @@ document.onkeyup = function (event) {
 
 
 
-//$("#startButton").onKeyUp("click", function() {
-    console.log("START BUTTON CLICKED")
-    document.onkeyup = gameStart();
-    var gameStart = function startGame() {
-
-var band = bands[Math.floor(Math.random()*bands.length)];
-console.log("The band is" + band);
-
-var wordLength = band.length;
-console.log("There are " + wordLength + " letters in the word")
-
-var splitWordArray = band.split("")
-
-var guessNumber = (wordLength + 10);
-document.write("you have " + guessNumber + "guesses")
-for(var i=0; i<wordLength; i++)
-document.write(hangDiv);
-document.write(splitWordArray[i]);
-document.write(hangDiv.appendChild(splitWordArray));
-
-
-
-
-    }
-//function randomnumber();
-   // var rand = Math.floor(Math.random()14);
 
 
 
