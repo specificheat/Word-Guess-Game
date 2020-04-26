290f70b7bec93691cd71baa88d43085f70055c9a
Jeremy Pena <jpena.boots@gmail.com>
2020-04-25 19:57:14 -0700

    final game code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var bands = ["timbertimbre", "neonindian", "wilco", "felakuti", "destroyer", "beak", "liars", "toroymoi", "grizzlybear", "deerhoof", "radiohead", "kauf", "holyghost", "warpaint", "kurtvile", "joydivision", "talkingheads", "lcdsoundsystem", "can", "prince"];
 
-//Empty variables to store values later
+
 var randomBand = "";
 var lettersOfBand = []
 var blanks = 0;
@@ -8,58 +8,54 @@ var blanksAndCorrect = [];
 var wrongGuess = [];
 
 
-//Counter Variables
+
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 9;
 
 function Game() {
-    //computer generates random word from words array
+    
     randomBand = bands[Math.floor(Math.random() * bands.length)];
 
-    // split the individual word into separate arrays, and store in new array 
+    
     lettersOfBand = randomBand.split("");
 
-    //store length of word in blanks, for later use
+    
     blanks = lettersOfBand.length;
 
-    //creating a loop to generate "_" for each letter in array stored in blanks
+    
     for (var i = 0; i < blanks; i++) {
         blanksAndCorrect.push("_");
     }
 
-    //showing the "_" within HTML
-   // window.onload = function(){
-        //what();
-        //function what(){
+    
     document.getElementById("currentword").innerHTML = ` ${blanksAndCorrect.join(" ")}`;
         };
-//}
-    //console logging 
+
     console.log(randomBand);
     console.log(lettersOfBand)
     console.log(blanks)
     console.log(blanksAndCorrect)
-//}
+
 function reset() {
     guessesRemaining = 9;
     wrongGuess = [];
     blanksAndCorrect = [];
     Game()
 }
-//If/Else, to see if letter selected matches random word
+
 function checkLetters(letter) {
     var letterInBand = false;
-    //if the generated randomword is equal to the letter entered... then variable is true
+    
     for (var i = 0; i < blanks; i++) {
         if (randomBand[i] == letter) {
             letterInBand = true;
             
         }
     }
-    //if letterInWord (false)
+   
     if (letterInBand) {
-        //check each letter to see if it matches word
+        
         for (var i = 0; i < blanks; i++) {
             if (randomBand[i] == letter) {
                 blanksAndCorrect[i] = letter;
@@ -68,10 +64,10 @@ function checkLetters(letter) {
 
         }
     }
-    //otherwise, push the incorrect guess in the wrong guesses section, and reduce remaining guesses
+    
     else {
         wrongGuess.push(letter);
-        guessesRemaining --;
+        guessesRemaining--;
     }
    console.log(blanksAndCorrect);
 }
@@ -79,44 +75,42 @@ function checkLetters(letter) {
 function complete() {
     console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
 
-    //if WON...then alert, play audio, display image and reset new round
+    
     if (lettersOfBand.toString() == blanksAndCorrect.toString()) {
         document.getElementById("band").innerHTML = `${randomBand}`;
         wins++;
         reset()
-        //display wins on screen
+        
         document.getElementById("winstracker").innerHTML = `${wins}`;
 
-        //if LOST...then alert and reset new round
-    } else if (guessesRemaining === 0) {
         
+    } else if (guessesRemaining === 0) {
+        document.getElementById("band").innerHTML = 'maybe try again, or take a nap';
         losses++;
         reset()
-        //window.onload = function(){
-            //what();
-            //function what(){
-        //document.getElementById("image").src = "./assets/images/try-again.png"
+        
+        
         document.getElementById("losstracker").innerHTML = `${losses}`;
             }
     }
-    //display losses on screen && guesses remaining countdown
+    
     document.getElementById("currentword").innerHTML = `${blanksAndCorrect.join(" ")}`;
-    document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
-//}
+    document.getElementById("guessesremaining").innerHTML = `${guessesRemaining}`;
+
 
 Game()
 
-//check for keyup, and convert to lowercase then store in guesses
+
 document.onkeyup = function (event) {
     var guesses = String.fromCharCode(event.keyCode).toLowerCase();
-    //check to see if guess entered matches value of random word
+   
     checkLetters(guesses);
-    //process wins/loss 
+   
     complete();
-    //store player guess in console for reference 
+     
     console.log(guesses);
 
-    //display/store incorrect letters on screen
+    
     document.getElementById("playerguesses").innerHTML = `${wrongGuess.join(" ")}`;
 }
-//};
+
